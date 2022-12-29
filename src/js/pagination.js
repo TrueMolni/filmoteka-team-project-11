db3c081ae6e872182c103b6f706247832319b52e
Mariia Makarkina <mariiamak25@gmail.com>
2022-12-29 22:42:16 +0200

    add functional of pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,53 +1,77 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+import ApiServise from '../js/api'
 
-const totalCount = 700;
+const apiServise = new ApiServise;
+const container = document.getElementById('tui-pagination-container');
+const containerFilms=document.querySelector(".container-films")
+// const page = pagination.getCurrentPage();
 // const arrowIcon = `${sprite}#icon-arrow`;
-// const dotsIcon = `${sprite}#icon-dots`;
+
+
+
 
 const options = {
-  page:1,
-  itemsPerPage:21,
-    totalItems: totalCount,
-    visiblePages: 5,
-    // centerAlign: true,
-    usageStatistics: false,
-    template:  {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
-      //  moveButton: '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">'+'<span class="tui-ico-{{type}}">1111</span>'+'</a>',
-      //   disabledMoveButton: '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">'+'<span class="tui-ico-{{type}}">{{type}}</span>'+'</span>',
-      //   moreButton: '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">'+'<span class="tui-ico-ellip">...</span>'+'</a>',
+  page: 1,
+  itemsPerPage: 20,
+  totalItems: 20000,
+  visiblePages: 5,
+  // centerAlign: true,
+  usageStatistics: false,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>'
   }
+}
 
-const container = document.getElementById('tui-pagination-container');
+
 const pagination = new Pagination(container, options);
 
-const page= pagination.getCurrentPage();
+async function Home() {
+  const response = await apiServise.getTrendingFilm();
+  addFilms(response.results);
+  console.log(response)
+}
+Home()
+
 
 
 pagination.on('afterMove', loadMoreFilms)
-  
-function loadMoreFilms(event) {
-const currentPage = event.page;
-  console.log(currentPage);
-  try {
-    
-  } catch (error) {
-    
-  }
 
-};
+async function loadMoreFilms(event) {
+  cleanContainer();
+
+  if (event.page) {
+    apiServise.page = event.page;
+ } 
+  console.log(event.page);
+  const response = await apiServise.getTrendingFilm();
+  addFilms(response.results);
+}
+
+
+function addFilms(films) {
+const markup = films.map(film => {
+  return `<div>
+<p>${film.title}</p>
+ </div>`}).join('');
+containerFilms.insertAdjacentHTML('beforeend', markup);
+
+}
+
+function cleanContainer(){
+  containerFilms.innerHTML = '';
+}
+

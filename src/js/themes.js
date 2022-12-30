32928aa828bfe8825833fdcaad93c1758c9b899c
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2022-12-30 23:00:08 +0200

    codes for light and dark themes

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,12 +1,10 @@
 const refs = {
   iconSun: document.querySelector('[data-action="light"]'),
   iconMoon: document.querySelector('[data-action="dark"]'),
-  //   textCard: document.querySelectorAll('.item__capt'), //! Не чипляється до H2
   footerContainer: document.querySelector('.footer'),
   footerText: document.querySelector('.footer .container p'),
+  body: document.querySelector('.themes'),
 };
-
-console.log(5);
 
 refs.iconMoon.addEventListener('click', onDarkTheme);
 
@@ -14,9 +12,7 @@ function onDarkTheme() {
   document.body.style.backgroundColor = '#252525';
   refs.footerContainer.style.backgroundColor = '#252525';
   refs.footerContainer.style.color = '#ffffff';
-  document.body.style.сolor = '#ffffff';
-
-  //   refs.textCard.style.color = '#ffffff';
+  refs.body.style.color = '#ffffff';
 
   refs.iconMoon.removeEventListener('click', onDarkTheme);
   refs.iconSun.addEventListener('click', onLightTheme);
@@ -28,8 +24,7 @@ function onLightTheme() {
   document.body.style.backgroundColor = '#ffffff';
   refs.footerContainer.style.backgroundColor = '#f7f7f7';
   refs.footerContainer.style.color = '#000000';
-  document.body.style.сolor = '#000000';
-  //   refs.textCard.style.color = '#000000';
+  refs.body.style.color = '#000000';
 
   refs.iconSun.removeEventListener('click', onLightTheme);
   refs.iconMoon.addEventListener('click', onDarkTheme);

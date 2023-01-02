ee72593a4ee98e848c441aa0f36909218021352c
TrueMolni <truemolni@gmail.com>
2023-01-02 22:26:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { getTrending } from './js/getTrending.js';
 import { getById } from './js/getById';
 import { createMarkup } from './js/markupListMovies.js';
 import './js/themes';
-import './js/searchByKeyWord'
+import './js/searchByKeyWord';
 import './js/modalOurTeam.js';
-import './js/local-storage/localStoragе';
-import './js/local-storage/libraryButtons';
+// import './js/local-storage/localStoragе';
+// import './js/local-storage/libraryButtons';
 import Spinner from './js/loader';

601d7cab76c19b0ba38e767411cd638a4cd04265
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-01-03 15:16:26 +0200

    додоає функції та слухачів подій для закриття модалки при кліку в бекдроп або  натисканню ESC

--- src/js/modalOurTeam.js
+++ src/js/modalOurTeam.js
@@ -1,14 +1,27 @@
-(() => {
-  const refs = {
+const refs = {
     openModalBtn: document.querySelector("[data-open-modal_our-team]"),
     closeModalBtn: document.querySelector("[data-close-modal_our-team]"),
     modal: document.querySelector("[data-modal_our-team]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+}
+
+refs.modal.addEventListener('click', closeModal);
+document.addEventListener('keydown', closeModalKeyESC);
+
+function closeModal(event) {
+  if (event.target.classList.contains('backdropp')){
+    refs.modal.classList.toggle("is-hidden")
   }
-})();
+}
+
+function closeModalKeyESC(event) {
+  if (event.key === "Escape") {
+    refs.modal.classList.toggle("is-hidden")
+  }
+}

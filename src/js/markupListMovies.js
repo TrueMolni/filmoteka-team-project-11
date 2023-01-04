a03772e10743cfcbcfb9c7bd6f5ff7d70e80197b
DarynaF <karlu4ka333@gmail.com>
2023-01-04 20:26:02 +0300

    Update markupListMovies.js

--- src/js/markupListMovies.js
+++ src/js/markupListMovies.js
@@ -1,5 +1,6 @@
 import { getTrending } from "./api.js";
 //import { getTrending } from './getTrending.js';
+import { getById } from "./api.js";
 
 const srcImgBase = 'https://image.tmdb.org/t/p/w500';
 let currentPage = 1;
@@ -13,8 +14,25 @@ const moviesList = document.querySelector('.film__list');
 //     moviesList.insertAdjacentHTML('beforeend', createMarkup(res));
 //   }
 // });
+import { getById } from "./api.js";
+async function getGenres() {
+  try {
+    const resGenres = await axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+    );
+    return await resGenres.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-
+async function renderGernes() {
+  if (localStorage.getItem('genres') === null) {
+    await getGenres().then(({ genres }) => {
+      localStorage.setItem('genres', JSON.stringify(genres));
+    });
+  }
+}
 
 function getGenreById(genreId, genresArray) {
   const genres = genresArray.find(option => option.id === genreId);
@@ -27,20 +45,20 @@ function createMarkup(res) {
     const markup = res.results
       .map(
         ({ id, title, poster_path, genre_ids, release_date, vote_average }) => {
-          //
-          // const genresList = JSON.parse(localStorage.getItem('genres'));
-          // let genres = genre_ids.map(item => {
-          // return getGenreById(item, genresList);
-          // })
 
-          // let genresMarkup = '';
-          // if (genres.length === 0) {
-          //  genresMarkup = 'No genres';
-          // } else if (genres.length < 3) {
-          //  genresMarkup = genres.join(',&nbsp;');
-          // } else {
-          //  genresMarkup = `${genres[0]}, ${genres[1]}, Others`;
-          // }
+          const genresList = JSON.parse(localStorage.getItem('genres'));
+           let genres = genre_ids.map(item => {
+          return getGenreById(item, genresList);
+          })
+
+          let genresMarkup = '';
+          if (genres.length === 0) {
+            genresMarkup = 'No genres';
+           } else if (genres.length < 3) {
+          genresMarkup = genres.join(',&nbsp;');
+          } else {
+           genresMarkup = `${genres[0]}, ${genres[1]}, Others`;
+          }
 
           // check for poster
           let poster = '';
@@ -65,7 +83,7 @@ function createMarkup(res) {
             <div class="item__text">
               <h2 class="item__capt">${title}</h2>
               <div class="item__wrap">
-                <p class="item__genre">${genre_ids}</p>
+                <p class="item__genre">${genres}</p>
                 <p class="item__rating">${voteAverage}</p>
                 <p class="item__genre">${relDate}</p>
               </div>

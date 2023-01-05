a9cfc42f4251d233fdc64375b13967bb9f253f0a
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-01-05 18:19:08 +0200

    змінює назви ключів в refs urTeam

--- src/js/modalOurTeam.js
+++ src/js/modalOurTeam.js
@@ -1,42 +1,42 @@
 const refs = {
-  openModalBtn: document.querySelector("[data-open-modal_our-team]"),
-  closeModalBtn: document.querySelector("[data-close-modal_our-team]"),
-  modal: document.querySelector("[data-modal_our-team]"),  
+  openModalOurTeam: document.querySelector("[data-open-modal_our-team]"),
+  closeModalOurTeam: document.querySelector("[data-close-modal_our-team]"),
+  modalOurTeam: document.querySelector("[data-modal_our-team]"),  
     
   socialIcons: document.querySelectorAll('.social-icons'),
   listEmployeItem: document.querySelectorAll('.list-employees-item'),
   titleModalOurTeam: document.querySelector('.title_modal_our-team'),
   iconClose: document.querySelector('.icon-close'),
-  modalOurTeam: document.querySelector('.modal_our-team'),
+  modalTeam: document.querySelector('.modal_our-team'),
   socialIconsDisabled: document.querySelectorAll('.social-icons__disabled'),
 
 };
 
-refs.openModalBtn.addEventListener("click", openModalOurTeam);
+refs.openModalOurTeam.addEventListener("click", openModalOurTeam);
 
 function openModalOurTeam() {
-  refs.modal.classList.remove('is-hidden'); 
+  refs.modalOurTeam.classList.remove('is-hidden'); 
   addEventListeners();
 }
 
 function closeModal(event) {
   if (event.target.classList.contains('backdropp') || event.key === "Escape"
     || event.currentTarget.classList.contains('button-close-modal')) {
-    refs.modal.classList.add('is-hidden');
+    refs.modalOurTeam.classList.add('is-hidden');
     removeEventListeners();
   }
 }
 
 function removeEventListeners() {
   document.removeEventListener('keydown', closeModal);
-  refs.modal.removeEventListener('click', closeModal);
-  refs.closeModalBtn.removeEventListener('click', closeModal);
+  refs.modalOurTeam.removeEventListener('click', closeModal);
+  refs.closeModalOurTeam.removeEventListener('click', closeModal);
 }
 
 function addEventListeners() {
   document.addEventListener('keydown', closeModal);
-  refs.modal.addEventListener('click', closeModal);
-  refs.closeModalBtn.addEventListener('click', closeModal);
+  refs.modalOurTeam.addEventListener('click', closeModal);
+  refs.closeModalOurTeam.addEventListener('click', closeModal);
 }
 
 function chengeDarkThemModalOurTeam({socialIcons, listEmployeItem, titleModalOurTeam, iconClose,modalOurTeam, socialIconsDisabled }) {
@@ -44,7 +44,7 @@ function chengeDarkThemModalOurTeam({socialIcons, listEmployeItem, titleModalOur
   socialIconsDisabled.forEach(element => element.classList.add('social-icons__disabled--themDark'));
   listEmployeItem.forEach(element => element.classList.add('list-employees-item__themDark'));
   iconClose.classList.add('icon-close__themDark');
-  modalOurTeam.classList.add('modal_our-team__themDark');
+  modalTeam.classList.add('modal_our-team__themDark');
   titleModalOurTeam.classList.add('title_modal_our-team__themDark');
 
 };
@@ -56,7 +56,7 @@ function chengeLightThemModalOurTeam({ socialIcons, listEmployeItem, titleModalO
   socialIconsDisabled.forEach(element => element.classList.remove('social-icons__disabled--themDark'));
   listEmployeItem.forEach(element => element.classList.remove('list-employees-item__themDark'));
   iconClose.classList.remove('icon-close__themDark');
-  modalOurTeam.classList.remove('modal_our-team__themDark');
+  modalTeam.classList.remove('modal_our-team__themDark');
   titleModalOurTeam.classList.remove('title_modal_our-team__themDark');
 };
 

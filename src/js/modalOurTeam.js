6c368cc760429aed8172648f7b9780fec8137a0f
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2023-01-05 18:01:42 +0200

    редагує функціі відкриття та закриття модалки, додає 2 функціі для зміни теми иодального вікна

--- src/js/modalOurTeam.js
+++ src/js/modalOurTeam.js
@@ -1,7 +1,17 @@
+
+
 const refs = {
-    openModalBtn: document.querySelector("[data-open-modal_our-team]"),
-    closeModalBtn: document.querySelector("[data-close-modal_our-team]"),
-    modal: document.querySelector("[data-modal_our-team]"),  
+  openModalBtn: document.querySelector("[data-open-modal_our-team]"),
+  closeModalBtn: document.querySelector("[data-close-modal_our-team]"),
+  modal: document.querySelector("[data-modal_our-team]"),  
+    
+  socialIcons: document.querySelectorAll('.social-icons'),
+  listEmployeItem: document.querySelectorAll('.list-employees-item'),
+  titleModalOurTeam: document.querySelector('.title_modal_our-team'),
+  iconClose: document.querySelector('.icon-close'),
+  modalOurTeam: document.querySelector('.modal_our-team'),
+  socialIconsDisabled: document.querySelectorAll('.social-icons__disabled'),
+
 };
 
 refs.openModalBtn.addEventListener("click", openModalOurTeam);
@@ -22,7 +32,7 @@ function closeModal(event) {
 function removeEventListeners() {
   document.removeEventListener('keydown', closeModal);
   refs.modal.removeEventListener('click', closeModal);
-  refs.closeModalBtn.removeEventListener('click', closeModalButton);
+  refs.closeModalBtn.removeEventListener('click', closeModal);
 }
 
 function addEventListeners() {
@@ -30,3 +40,30 @@ function addEventListeners() {
   refs.modal.addEventListener('click', closeModal);
   refs.closeModalBtn.addEventListener('click', closeModal);
 }
+
+
+
+function chengeDarkThemModalOurTeam({socialIcons, listEmployeItem, titleModalOurTeam, iconClose,modalOurTeam, socialIconsDisabled }) {
+  socialIcons.forEach(element => element.classList.add('social-icons__themDark'));
+  socialIconsDisabled.forEach(element => element.classList.add('social-icons__disabled--themDark'));
+  listEmployeItem.forEach(element => element.classList.add('list-employees-item__themDark'));
+  iconClose.classList.add('icon-close__themDark');
+  modalOurTeam.classList.add('modal_our-team__themDark');
+  titleModalOurTeam.classList.add('title_modal_our-team__themDark');
+
+};
+
+
+
+  
+
+function chengeLightThemModalOurTeam({ socialIcons, listEmployeItem, titleModalOurTeam, iconClose, modalOurTeam, socialIconsDisabled }) {
+  socialIcons.forEach(element => element.classList.remove('social-icons__themDark'));
+  socialIconsDisabled.forEach(element => element.classList.remove('social-icons__disabled--themDark'));
+  listEmployeItem.forEach(element => element.classList.remove('list-employees-item__themDark'));
+  iconClose.classList.remove('icon-close__themDark');
+  modalOurTeam.classList.remove('modal_our-team__themDark');
+  titleModalOurTeam.classList.remove('title_modal_our-team__themDark');
+};
+
+

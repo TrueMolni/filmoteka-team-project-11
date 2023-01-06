6b6166bd468d44a2c4c5a3ce5065d8fc8fa25410
D. Akelar <108392983+akelarka@users.noreply.github.com>
2023-01-07 01:05:12 +0200

    finally_fixed_modal_buttons

--- src/js/changeModalBtn.js
+++
@@ -1,7 +0,0 @@
-export function changeBtnStyle (targetBtn, keyLS) {
-    // console.log(isAdded)
-    targetBtn.style.color = isAdded ? '#fff' : '';
-    targetBtn.style.backgroundColor = isAdded ? '#ff6b08' : '';
-    targetBtn.textContent = isAdded ? `remove from ${keyLS}` : `add to ${keyLS}`
-    isAdded = false;
-}

b86cc5cb707df13307a9249deae680d9a34d9373
TrueMolni <truemolni@gmail.com>
2023-01-09 11:35:26 +0200

    0.1.1

--- src/js/findAndAuditLS.js
+++ src/js/findAndAuditLS.js
@@ -1,42 +1,40 @@
-import {getById} from './api';
-import {parseOneFilm} from './parseApiToLS';
+import { getById } from './api';
+import { parseOneFilm } from './parseApiToLS';
 
 let cardItem = null;
 
-export function getFilmData (targetCardId, base) {
-    try {
-        const localStorageArray = JSON.parse(localStorage.getItem(base));
-        cardItem = findAndAdd(localStorageArray, targetCardId);
-        if (cardItem) {
-            isAdded = true;
-            return cardItem;
-        }
-        return findCardItem(targetCardId).then(cardItem => {
-            return cardItem;
-        });
-    } catch (err) {
-        console.error('Get LocslStorage error: ', err);
+export function getFilmData(targetCardId, base) {
+  try {
+    const localStorageArray = JSON.parse(localStorage.getItem(base));
+    cardItem = findAndAdd(localStorageArray, targetCardId);
+    if (cardItem) {
+      isAdded = true;
+      return cardItem;
     }
+    return findCardItem(targetCardId).then(cardItem => {
+      return cardItem;
+    });
+  } catch (err) {
+    console.error('Get LocslStorage error: ', err);
+  }
 }
 
-function findCardItem (targetCardId) {
-    return getById(targetCardId).then(data => {
-        // console.log(data);
-        cardItem = data;
-        return parseOneFilm(cardItem);
-    }
-    );
+function findCardItem(targetCardId) {
+  return getById(targetCardId).then(data => {
+    cardItem = data;
+    return parseOneFilm(cardItem);
+  });
 }
 
-function findAndAdd (currentArray, targetCardId) {
-    if (currentArray === null) {
-        currentArray = [];
+function findAndAdd(currentArray, targetCardId) {
+  if (currentArray === null) {
+    currentArray = [];
+  }
+  let cardItem;
+  currentArray.forEach(item => {
+    if (item.id === Number(targetCardId)) {
+      cardItem = item;
     }
-    let cardItem;
-    currentArray.forEach(item => {
-        if (item.id === Number(targetCardId)) {
-            cardItem = item;
-        }
-        return cardItem;
-    })
+    return cardItem;
+  });
 }
